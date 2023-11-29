c791b81ba19daf44af7fb192fb51485c280635e4
Felipe Freitas <eu@felipefs.dev>
2023-11-29 01:45:51 -0300

    Function Prototypes and reorder

--- Trabalho02/ProgramaBasico3D.cpp
+++ Trabalho02/ProgramaBasico3D.cpp
@@ -61,6 +61,26 @@ int ModoDeExibicao = 1;
 double nFrames = 0;
 double TempoTotal = 0;
 
+// Prototipos
+void DesenhaChao();
+void DesenhaParedao();
+void DesenhaLadrilho(int corBorda, int corDentro);
+void DesenhaParalelepipedo(float largura, float altura, float profundidade);
+void DesenhaVeiculo();
+void moveVeiculo(unsigned char key);
+void rotacionaVeiculo(unsigned char key);
+void rotacionaCanhao(unsigned char key);
+void atiraProjetil();
+void init(void);
+void display(void);
+void PosicUser();
+void keyboard(unsigned char key, int x, int y);
+void arrow_keys(int a_keys, int x, int y);
+void animate();
+void MygluPerspective(float fieldOfView, float aspect, float zNear, float zFar);
+void DefineLuz(void);
+void reshape(int w, int h);
+
 // Constants
 const int wallHeight = 15;
 const int sceneWidth = 25;
@@ -82,159 +102,20 @@ Ponto AnguloCanhao;
 Ponto DirecaoCanhao;
 float forcaCanhao;
 
-void init(void) {
-    glClearColor(0.6156862745f, 0.8980392157f, 0.9803921569f, 1.0f);
-    // Fundo de tela azul claro
-
-    glClearDepth(1.0);
-    glDepthFunc(GL_LESS);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glEnable(GL_NORMALIZE);
-    glShadeModel(GL_SMOOTH);
-    // glShadeModel(GL_FLAT);
-
-    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-    // Faces Preenchidas??
-    if (ModoDeExibicao)
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    else
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    // Reset wall grid
-    PosicaoParedao = Ponto(0, -1, sceneDepth / 2);
-    for (int i = 0; i < sceneWidth; i++)
-        for (int j = 0; j < wallHeight; j++)
-            wallGrid[i][j] = true;
-
-    TamanhoVeiculo = Ponto(2, 1, 3);
-    distanciaMovimentoVeiculo = 1.0f;
-    // TamanhoCanhao = Ponto((1/4.0) * TamanhoVeiculo.x,
-    //                       (1/5.0) * TamanhoVeiculo.y,
-    //                       (2/3.0) * TamanhoVeiculo.z);
-    TamanhoCanhao = Ponto(0.5, 0.5, 2.0);
-    PosicaoVeiculo = Ponto(6, 0, 4);
-    AnguloVeiculo = Ponto(0, 0, 0);
-    DirecaoVeiculo = Ponto(0, 0, 1);
-    PosicaoRelativaCanhao = Ponto(0, 0.5, 1);
-    AnguloCanhao = Ponto(0, 0, 0);
-    DirecaoCanhao = Ponto(0, 0, 1);
-    forcaCanhao = 1.0f;
-}
-
-void animate() {
-    double dt;
-    dt = T.getDeltaT();
-    AccumDeltaT += dt;
-    TempoTotal += dt;
-    nFrames++;
-
-    // fixa a atualizacao da tela em 30
-    if (AccumDeltaT > 1.0/30) {
-        AccumDeltaT = 0;
-        glutPostRedisplay();
-    }
-    if (TempoTotal > 5.0) {
-        /*
-            cout << "Tempo Acumulado: "  << TempoTotal << " segundos. " ;
-            cout << "Nros de Frames sem desenho: " << nFrames << endl;
-            cout << "FPS(sem desenho): " << nFrames/TempoTotal << endl;
-            TempoTotal = 0;
-            nFrames = 0;
-        */
-    }
-}
-
-void DesenhaCubo(float tamAresta) {
-    glBegin(GL_QUADS);
-    // Front Face
-    glNormal3f(0, 0, 1);
-    glVertex3f(-tamAresta/2, -tamAresta/2,  tamAresta/2);
-    glVertex3f( tamAresta/2, -tamAresta/2,  tamAresta/2);
-    glVertex3f( tamAresta/2,  tamAresta/2,  tamAresta/2);
-    glVertex3f(-tamAresta/2,  tamAresta/2,  tamAresta/2);
-    // Back Face
-    glNormal3f(0, 0, -1);
-    glVertex3f(-tamAresta/2, -tamAresta/2, -tamAresta/2);
-    glVertex3f(-tamAresta/2,  tamAresta/2, -tamAresta/2);
-    glVertex3f( tamAresta/2,  tamAresta/2, -tamAresta/2);
-    glVertex3f( tamAresta/2, -tamAresta/2, -tamAresta/2);
-    // Top Face
-    glNormal3f(0, 1, 0);
-    glVertex3f(-tamAresta/2,  tamAresta/2, -tamAresta/2);
-    glVertex3f(-tamAresta/2,  tamAresta/2,  tamAresta/2);
-    glVertex3f( tamAresta/2,  tamAresta/2,  tamAresta/2);
-    glVertex3f( tamAresta/2,  tamAresta/2, -tamAresta/2);
-    // Bottom Face
-    glNormal3f(0, -1, 0);
-    glVertex3f(-tamAresta/2, -tamAresta/2, -tamAresta/2);
-    glVertex3f( tamAresta/2, -tamAresta/2, -tamAresta/2);
-    glVertex3f( tamAresta/2, -tamAresta/2,  tamAresta/2);
-    glVertex3f(-tamAresta/2, -tamAresta/2,  tamAresta/2);
-    // Right face
-    glNormal3f(1, 0, 0);
-    glVertex3f( tamAresta/2, -tamAresta/2, -tamAresta/2);
-    glVertex3f( tamAresta/2,  tamAresta/2, -tamAresta/2);
-    glVertex3f( tamAresta/2,  tamAresta/2,  tamAresta/2);
-    glVertex3f( tamAresta/2, -tamAresta/2,  tamAresta/2);
-    // Left Face
-    glNormal3f(-1, 0, 0);
-    glVertex3f(-tamAresta/2, -tamAresta/2, -tamAresta/2);
-    glVertex3f(-tamAresta/2, -tamAresta/2,  tamAresta/2);
-    glVertex3f(-tamAresta/2,  tamAresta/2,  tamAresta/2);
-    glVertex3f(-tamAresta/2,  tamAresta/2, -tamAresta/2);
-    glEnd();
-}
-
-void DesenhaParalelepipedo(float largura, float altura, float profundidade) {
+void DesenhaChao() {
     glPushMatrix();
-        glTranslatef(0, 0, 0);
-        glScalef(largura, altura, profundidade);
-        DesenhaCubo(1);
+        glTranslated(0, -1, 0);
+        for (int x = 0; x < sceneWidth; x++) {
+            glPushMatrix();
+            for (int z = 0; z < sceneDepth; z++) {
+                float color = z < sceneDepth / 2 ? BlueViolet : IndianRed;
+                DesenhaLadrilho(MediumGoldenrod, color);
+                glTranslated(0, 0, 1);
+            }
+            glPopMatrix();
+            glTranslated(1, 0, 0);
+        }
     glPopMatrix();
-}
-
-void DesenhaVeiculo() {
-    glPushMatrix();
-        // Base 
-        glTranslatef(PosicaoVeiculo.x, PosicaoVeiculo.y, PosicaoVeiculo.z);
-        glRotatef(AnguloVeiculo.y, 0, 1, 0);
-        glColor3f(0.4f, 1.0f, 0.2f);
-        DesenhaParalelepipedo(TamanhoVeiculo.x, TamanhoVeiculo.y, TamanhoVeiculo.z);
-        
-        // Canhão
-        glTranslatef(PosicaoRelativaCanhao.x, PosicaoRelativaCanhao.y, PosicaoRelativaCanhao.z);
-        glRotatef(AnguloCanhao.x, 1, 0, 0);
-        glColor3f(1.0f, 0.2f, 0.1f);
-        DesenhaParalelepipedo(TamanhoCanhao.x, TamanhoCanhao.y, TamanhoCanhao.z);
-    glPopMatrix();
-}
-
-// **********************************************************************
-// void DesenhaLadrilho(int corBorda, int corDentro)
-// Desenha uma celula do piso.
-// E possivel definir a cor da borda e do interior do piso
-// O ladrilho tem largula 1, centro no (0, 0, 0) e esta sobre o plano XZ
-// **********************************************************************
-void DesenhaLadrilho(int corBorda, int corDentro) {
-    // Desenha quadrado preenchido
-    defineCor(corDentro);
-    glBegin(GL_QUADS);
-        glNormal3f(0,1,0);
-        glVertex3f(-0.5f,  0.0f, -0.5f);
-        glVertex3f(-0.5f,  0.0f,  0.5f);
-        glVertex3f( 0.5f,  0.0f,  0.5f);
-        glVertex3f( 0.5f,  0.0f, -0.5f);
-    glEnd();
-
-    defineCor(corBorda);
-    glBegin(GL_LINE_STRIP);
-        glNormal3f(0,1,0);
-        glVertex3f(-0.5f,  0.0f, -0.5f);
-        glVertex3f(-0.5f,  0.0f,  0.5f);
-        glVertex3f( 0.5f,  0.0f,  0.5f);
-        glVertex3f( 0.5f,  0.0f, -0.5f);
-    glEnd();
 }
 
 void DesenhaParedao() {
@@ -265,127 +146,49 @@ void DesenhaParedao() {
     glPopMatrix();
 }
 
-void DesenhaChao() {
+void DesenhaLadrilho(int corBorda, int corDentro) {
+    // Desenha quadrado preenchido
+    defineCor(corDentro);
+    glBegin(GL_QUADS);
+        glNormal3f(0,1,0);
+        glVertex3f(-0.5f,  0.0f, -0.5f);
+        glVertex3f(-0.5f,  0.0f,  0.5f);
+        glVertex3f( 0.5f,  0.0f,  0.5f);
+        glVertex3f( 0.5f,  0.0f, -0.5f);
+    glEnd();
+
+    defineCor(corBorda);
+    glBegin(GL_LINE_STRIP);
+        glNormal3f(0,1,0);
+        glVertex3f(-0.5f,  0.0f, -0.5f);
+        glVertex3f(-0.5f,  0.0f,  0.5f);
+        glVertex3f( 0.5f,  0.0f,  0.5f);
+        glVertex3f( 0.5f,  0.0f, -0.5f);
+    glEnd();
+}
+
+void DesenhaParalelepipedo(float largura, float altura, float profundidade) {
     glPushMatrix();
-        glTranslated(0, -1, 0);
-        for (int x = 0; x < sceneWidth; x++) {
-            glPushMatrix();
-            for (int z = 0; z < sceneDepth; z++) {
-                float color = z < sceneDepth / 2 ? BlueViolet : IndianRed;
-                DesenhaLadrilho(MediumGoldenrod, color);
-                glTranslated(0, 0, 1);
-            }
-            glPopMatrix();
-            glTranslated(1, 0, 0);
-        }
+        glTranslatef(0, 0, 0);
+        glScalef(largura, altura, profundidade);
+        glutSolidCube(1);
     glPopMatrix();
 }
 
-void DefineLuz(void) {
-    // Define cores para um objeto dourado
-    GLfloat LuzAmbiente[] = {0.4, 0.4, 0.4};
-    GLfloat LuzDifusa[] = {0.7, 0.7, 0.7};
-    GLfloat LuzEspecular[] = {0.9f, 0.9f, 0.9};
-    GLfloat PosicaoLuz0[] = {0.0f, 3.0f, 5.0f};
-    GLfloat Especularidade[] = {1.0f, 1.0f, 1.0f};
-
-    // ****************  Fonte de Luz 0
-    glEnable(GL_COLOR_MATERIAL);
-
-    // Habilita o uso de iluminacao
-    glEnable(GL_LIGHTING);
-
-    // Ativa o uso da luz ambiente
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, LuzAmbiente);
-    // Define os parametros da luz numero Zero
-    glLightfv(GL_LIGHT0, GL_AMBIENT, LuzAmbiente);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, LuzDifusa);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, LuzEspecular);
-    glLightfv(GL_LIGHT0, GL_POSITION, PosicaoLuz0);
-    glEnable(GL_LIGHT0);
-
-    // Ativa o "Color Tracking"
-    glEnable(GL_COLOR_MATERIAL);
-
-    // Define a reflectancia do material
-    glMaterialfv(GL_FRONT,GL_SPECULAR, Especularidade);
-
-    // Define a concentracao do brilho.
-    // Quanto maior o valor do Segundo parametro, mais
-    // concentrado sera o brilho. (Valores validos: de 0 a 128)
-    glMateriali(GL_FRONT, GL_SHININESS, 51);
-}
-
-void MygluPerspective(float fieldOfView, float aspect, float zNear, float zFar) {
-    // https://stackoverflow.com/questions/2417697/gluperspective-was-removed-in-opengl-3-1-any-replacements/2417756#2417756
-    // The following code is a fancy bit of math that is equivilant to calling:
-    // gluPerspective(fieldOfView/2.0f, width/height, 0.1f, 255.0f)
-    // We do it this way simply to avoid requiring glu.h
-    // GLfloat zNear = 0.1f;
-    // GLfloat zFar = 255.0f;
-    // GLfloat aspect = float(width)/float(height);
-    GLfloat fH = tan(float(fieldOfView / 360.0f * 3.14159f)) * zNear;
-    GLfloat fW = fH * aspect;
-    glFrustum(-fW, fW, -fH, fH, zNear, zFar);
-}
-
-void PosicUser() {
-    // Define os parametros da projecao Perspectiva
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    // Define o volume de visualizacao sempre a partir da posicao do observador
-    if (ModoDeProjecao == 0)
-        // Projecao paralela Orthografica
-        glOrtho(-10, 10, -10, 10, 0, 7);
-    // Projecao perspectiva
-    else MygluPerspective(90,AspectRatio,0.01,50);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(
-        PosicaoVeiculo.x - 5, PosicaoVeiculo.y + 1, PosicaoVeiculo.z,
-        PosicaoVeiculo.x, PosicaoVeiculo.y, PosicaoVeiculo.z,
-        // Vetor ViewUp
-        0.0f, 1.0f, 0.0f
-    );
-}
-
-void reshape(int w, int h) {
-	// Evita divisao por zero, no caso de uam janela com largura 0.
-	if (h == 0) h = 1;
-    // Ajusta a relacao entre largura e altura para evitar distorcao na imagem.
-    // Veja funcao "PosicUser".
-	AspectRatio = 1.0f * w / h;
-	// Reset the coordinate system before modifying
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	// Seta a viewport para ocupar toda a janela
-    glViewport(0, 0, w, h);
-    //cout << "Largura" << w << endl;
-
-	PosicUser();
-}
-
-float PosicaoZ = -30;
-void display(void) {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	DefineLuz();
-
-	PosicUser();
-
-	glMatrixMode(GL_MODELVIEW);
-
-    DesenhaChao();
-
-    DesenhaParedao();
-
-    DesenhaVeiculo();
-
-	glutSwapBuffers();
+void DesenhaVeiculo() {
+    glPushMatrix();
+        // Base 
+        glTranslatef(PosicaoVeiculo.x, PosicaoVeiculo.y, PosicaoVeiculo.z);
+        glRotatef(AnguloVeiculo.y, 0, 1, 0);
+        glColor3f(0.4f, 1.0f, 0.2f);
+        DesenhaParalelepipedo(TamanhoVeiculo.x, TamanhoVeiculo.y, TamanhoVeiculo.z);
+        
+        // Canhão
+        glTranslatef(PosicaoRelativaCanhao.x, PosicaoRelativaCanhao.y, PosicaoRelativaCanhao.z);
+        glRotatef(AnguloCanhao.x, 1, 0, 0);
+        glColor3f(1.0f, 0.2f, 0.1f);
+        DesenhaParalelepipedo(TamanhoCanhao.x, TamanhoCanhao.y, TamanhoCanhao.z);
+    glPopMatrix();
 }
 
 void moveVeiculo(unsigned char key) {
@@ -454,6 +257,88 @@ void atiraProjetil() {
     Ponto Alvo = PosicaoCanhao + Ponto(0, 0, Distancia);
 }
 
+void init(void) {
+    glClearColor(0.6156862745f, 0.8980392157f, 0.9803921569f, 1.0f);
+    // Fundo de tela azul claro
+
+    glClearDepth(1.0);
+    glDepthFunc(GL_LESS);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_NORMALIZE);
+    glShadeModel(GL_SMOOTH);
+    // glShadeModel(GL_FLAT);
+
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+    // Faces Preenchidas??
+    if (ModoDeExibicao)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    else
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // Reset wall grid
+    PosicaoParedao = Ponto(0, -1, sceneDepth / 2);
+    for (int i = 0; i < sceneWidth; i++)
+        for (int j = 0; j < wallHeight; j++)
+            wallGrid[i][j] = true;
+
+    TamanhoVeiculo = Ponto(2, 1, 3);
+    distanciaMovimentoVeiculo = 1.0f;
+    // TamanhoCanhao = Ponto((1/4.0) * TamanhoVeiculo.x,
+    //                       (1/5.0) * TamanhoVeiculo.y,
+    //                       (2/3.0) * TamanhoVeiculo.z);
+    TamanhoCanhao = Ponto(0.5, 0.5, 2.0);
+    PosicaoVeiculo = Ponto(6, 0, 4);
+    AnguloVeiculo = Ponto(0, 0, 0);
+    DirecaoVeiculo = Ponto(0, 0, 1);
+    PosicaoRelativaCanhao = Ponto(0, 0.5, 1);
+    AnguloCanhao = Ponto(0, 0, 0);
+    DirecaoCanhao = Ponto(0, 0, 1);
+    forcaCanhao = 1.0f;
+}
+
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	DefineLuz();
+
+	PosicUser();
+
+	glMatrixMode(GL_MODELVIEW);
+
+    DesenhaChao();
+
+    DesenhaParedao();
+
+    DesenhaVeiculo();
+
+	glutSwapBuffers();
+}
+
+void PosicUser() {
+    // Define os parametros da projecao Perspectiva
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    // Define o volume de visualizacao sempre a partir da posicao do observador
+    if (ModoDeProjecao == 0)
+        // Projecao paralela Orthografica
+        glOrtho(-10, 10, -10, 10, 0, 7);
+    // Projecao perspectiva
+    else MygluPerspective(90,AspectRatio,0.01,50);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(
+        PosicaoVeiculo.x - 5, PosicaoVeiculo.y + 1, PosicaoVeiculo.z,
+        PosicaoVeiculo.x, PosicaoVeiculo.y, PosicaoVeiculo.z,
+        // Vetor ViewUp
+        0.0f, 1.0f, 0.0f
+    );
+}
+
 void keyboard(unsigned char key, int x, int y) {
 	switch(key) {
         // Termina o programa qdo a tecla ESC for pressionada
@@ -512,6 +397,93 @@ void arrow_keys(int a_keys, int x, int y) {
 		default:
 			break;
 	}
+}
+
+void animate() {
+    double dt;
+    dt = T.getDeltaT();
+    AccumDeltaT += dt;
+    TempoTotal += dt;
+    nFrames++;
+
+    // fixa a atualizacao da tela em 30
+    if (AccumDeltaT > 1.0/30) {
+        AccumDeltaT = 0;
+        glutPostRedisplay();
+    }
+    if (TempoTotal > 5.0) {
+        /*
+            cout << "Tempo Acumulado: "  << TempoTotal << " segundos. " ;
+            cout << "Nros de Frames sem desenho: " << nFrames << endl;
+            cout << "FPS(sem desenho): " << nFrames/TempoTotal << endl;
+        */
+        TempoTotal = 0;
+        nFrames = 0;
+    }
+}
+
+void MygluPerspective(float fieldOfView, float aspect, float zNear, float zFar) {
+    // https://stackoverflow.com/questions/2417697/gluperspective-was-removed-in-opengl-3-1-any-replacements/2417756#2417756
+    // The following code is a fancy bit of math that is equivilant to calling:
+    // gluPerspective(fieldOfView/2.0f, width/height, 0.1f, 255.0f)
+    // We do it this way simply to avoid requiring glu.h
+    // GLfloat zNear = 0.1f;
+    // GLfloat zFar = 255.0f;
+    // GLfloat aspect = float(width)/float(height);
+    GLfloat fH = tan(float(fieldOfView / 360.0f * 3.14159f)) * zNear;
+    GLfloat fW = fH * aspect;
+    glFrustum(-fW, fW, -fH, fH, zNear, zFar);
+}
+
+void DefineLuz(void) {
+    // Define cores para um objeto dourado
+    GLfloat LuzAmbiente[] = {0.4, 0.4, 0.4};
+    GLfloat LuzDifusa[] = {0.7, 0.7, 0.7};
+    GLfloat LuzEspecular[] = {0.9f, 0.9f, 0.9};
+    GLfloat PosicaoLuz0[] = {0.0f, 3.0f, 5.0f};
+    GLfloat Especularidade[] = {1.0f, 1.0f, 1.0f};
+
+    // ****************  Fonte de Luz 0
+    glEnable(GL_COLOR_MATERIAL);
+
+    // Habilita o uso de iluminacao
+    glEnable(GL_LIGHTING);
+
+    // Ativa o uso da luz ambiente
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, LuzAmbiente);
+    // Define os parametros da luz numero Zero
+    glLightfv(GL_LIGHT0, GL_AMBIENT, LuzAmbiente);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, LuzDifusa);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, LuzEspecular);
+    glLightfv(GL_LIGHT0, GL_POSITION, PosicaoLuz0);
+    glEnable(GL_LIGHT0);
+
+    // Ativa o "Color Tracking"
+    glEnable(GL_COLOR_MATERIAL);
+
+    // Define a reflectancia do material
+    glMaterialfv(GL_FRONT,GL_SPECULAR, Especularidade);
+
+    // Define a concentracao do brilho.
+    // Quanto maior o valor do Segundo parametro, mais
+    // concentrado sera o brilho. (Valores validos: de 0 a 128)
+    glMateriali(GL_FRONT, GL_SHININESS, 51);
+}
+
+void reshape(int w, int h) {
+	// Evita divisao por zero, no caso de uam janela com largura 0.
+	if (h == 0) h = 1;
+    // Ajusta a relacao entre largura e altura para evitar distorcao na imagem.
+    // Veja funcao "PosicUser".
+	AspectRatio = 1.0f * w / h;
+	// Reset the coordinate system before modifying
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	// Seta a viewport para ocupar toda a janela
+    glViewport(0, 0, w, h);
+    //cout << "Largura" << w << endl;
+
+	PosicUser();
 }
 
 int main(int argc, char** argv) {
